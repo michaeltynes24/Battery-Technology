cf98b09e179a150c1c38b618bf4801b1c1512d95
Michael Tynes <mtynes6820@sdsu.edu>
2024-03-23 22:10:22 -0700

    initial setup with react and bootstrap

---
+++ react-battery-tech/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const webSiteName = 'VAM2S Solarshine'
+const engineers = ['Michael', 'Vishal', 'Alex', 'Steven', 'Maria']
+const loggedIn = true
+
+
+const App = () => {
+  return (
+    <>    
+
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/images/teamLogo.png" width="60" height="30" alt=""></img>
+      {webSiteName}</a>
+  </nav>
+
+  <div>
+    { loggedIn ? <h2>Hello Member</h2> : ''}
+    </div>
+
+    
+  </>
+
+  )
+}
+
+export default App
