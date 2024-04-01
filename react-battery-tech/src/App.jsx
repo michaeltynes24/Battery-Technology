9cc2dff7d18e27ca7d705a85007b78518c9e8365
Michael Tynes <mtynes6820@sdsu.edu>
2024-03-31 20:44:25 -0700

    added react-router-dom, added history,optimizer,profile,savings, login page files

--- react-battery-tech/src/App.jsx
+++ react-battery-tech/src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import CardStack from './components/CardStack';
 import Footer from './components/Footer';
