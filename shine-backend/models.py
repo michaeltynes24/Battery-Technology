101cccdf0bb761a297933a3af46b8263b3e7ec8e
STEVEN BUSSE <sbusse6231@sdsu.edu>
2024-04-08 13:54:11 -0700

    Added funcitonality to user page
    this may break the backend I need to do a little testinf

--- shine-backend/models.py
+++ shine-backend/models.py
@@ -1,13 +1,15 @@
-
+# models.py
 from flask_sqlalchemy import SQLAlchemy
-
-
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    energy_provider = db.Column(db.String(100), nullable=False)
-    # Add more columns as needed
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    battery_type = db.Column(db.String(50), nullable=False)
+    utility_company = db.Column(db.String(50), nullable=False)
+    import_energy_data = db.Column(db.Boolean, default=False)
+    battery_size = db.Column(db.Integer, nullable=False)
+    has_solar = db.Column(db.Boolean, default=False)
