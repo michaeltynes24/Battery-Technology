84a8ab6fc1e3990b64cd265181a072db958ba402
mariadawood <159504533+mariadawood@users.noreply.github.com>
2024-10-14 19:27:21 -0700

    serializers.py

---
+++ backend/ShineApp/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validation_data):
+        user = User.objects.create_user(**validate_data)
+        return user
